cd6d84ec0ae1782daf77aea32107ec21e656b539
FilipKosowski <filipkosowski1234@gmail.com>
2026-06-07 17:50:01 +0200

    Graczv1 z movmentem dodanym

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ const int WINDOW_HEIGHT = 820;
 //colory
 sf::Color TileColor(70, 90 ,100);
 sf::Color TileOutline(255 ,255,255);
+sf::Color playerOutline(0, 0, 80);
 
 struct Cell {
     bool mine = false;
@@ -28,6 +29,9 @@ struct Difficultylvl {
     int mines;
 };
 
+//gracz
+int playerRow = 0;
+int playerCol = 0;
 
 
 
@@ -62,9 +66,7 @@ int main()
         }
 
         window.clear(sf::Color(0, 0, 0));
-
-
-        //podzial na czesci
+        //generowanie mapy
         int cellsize = 40;
         int startX = 350;
         int startY = 150;
@@ -80,6 +82,35 @@ int main()
                 window.draw(tile);
             }
         }
+
+        //palyer
+        //movment
+
+            if(event.type == sf::Event::KeyPressed) {
+                if((event.key.code == sf::Keyboard::Left || event.key.code == sf::Keyboard::A) && playerCol > 0) {
+                    playerCol--;
+                }
+                else if((event.key.code == sf::Keyboard::Right || event.key.code == sf::Keyboard::D) && playerCol < level.cols - 1) {
+                    playerCol++;
+                }
+                else if((event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::W) && playerRow > 0) {
+                    playerRow--;
+                }
+                else if((event.key.code == sf::Keyboard::Down || event.key.code == sf::Keyboard::S) && playerRow < level.rows - 1) {
+                    playerRow++;
+                }
+            }
+
+        //draw player
+        sf::RectangleShape player(sf::Vector2f(cellsize-4, cellsize -4));
+        player.setPosition( startX + playerCol * cellsize +1, startY + playerRow * cellsize +1);
+        player.setFillColor(sf::Color::Transparent);
+        player.setOutlineColor(playerOutline);
+        player.setOutlineThickness(5);
+        window.draw(player);
+
+
+
 
 
 
