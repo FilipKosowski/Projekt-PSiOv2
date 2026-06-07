d6b239a47a9f70cdc8d4bf3a3c19145999efe053
FilipKosowski <filipkosowski1234@gmail.com>
2026-06-07 16:52:14 +0200

    Okno_poporstu_ale_dziala

--- main.cpp
+++ main.cpp
@@ -2,11 +2,35 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
-#include <iostream>
+
+const int WINDOW_WIDTH = 1180;
+const int WINDOW_HEIGHT = 820;
 
 
 int main()
 {
-    std::cout<< 5;
+    sf::RenderWindow window(
+        sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+        "Saper SFML"
+        );
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()){
+        sf::Event event {};
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(20, 25, 18));
+        window.display();
+    }
+
+
+
+
+
     return 0;
 }
